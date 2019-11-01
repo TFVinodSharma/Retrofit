c2b676858f8440af49693a0fc1587c57a4624651
Dell <vinodsharma9643@gmail.com>
2019-11-01 21:27:54 +0530

    Second Commit

--- app/src/main/java/vinod/learning/com/retrofit/MainActivity.java
+++ app/src/main/java/vinod/learning/com/retrofit/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<List<RetroPhoto>> call, Throwable t) {
                 progressDoalog.dismiss();
+                
                 Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
             }
         });
